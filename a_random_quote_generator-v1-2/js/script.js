133504da2376fed28bbf723ec8082e44b23ccbf3
CarlosCampos10 <53207200+CarlosCampos10@users.noreply.github.com>
2019-07-26 22:44:57 -0700

    Update script.js

--- a_random_quote_generator-v1-2/js/script.js
+++ a_random_quote_generator-v1-2/js/script.js
@@ -1,5 +1,5 @@
- 
-//These are my quotes put inside an array//
+ //These are my quotes put inside an array//
+// 4 in total my 5th on is im the index.html to start//
 let quotes = [
   {
   quote:"You miss 100 percent of the shots you dont take", 
@@ -21,9 +21,10 @@ let quotes = [
   },
 ];
 
-//This function gets a random Quote by first getting a random Number and then searching the index of the quotes array for that object.
 let storedNum;
 
+  //this function selects a random quote from 1-4//
+  //return random quote//
 function getRandomQuote() {
   let randomNum = Math.floor(Math.random() * 4);
   if (randomNum === storedNum) {
@@ -34,23 +35,17 @@ function getRandomQuote() {
   }
   return quotes[randomNum];
 }
-//Background color array containing css colors.
+//I got these color codes online but im still having trouble because they wont change //
+//the background color//
 let colors = ['E633FF', '070E07' , 'FFF933', '337DFF', 'FF3333'];
 
-//Function to pull a random color from the background color array.
+//Function to choose a color at random//
 function randomColor() {
   let randomNum2 = Math.floor(Math.random() * 4);
   return colors[randomNum2];
-
 }
-
-/*
-This function creates an empty html string.
-It then uses the getRandomQuote function to select a random quote from the quote array.
-It starts to compile a string of html from the different key/value pairs associated with that random quote.  Once the html is all concatenated, it will then print that quote to the webpage.
-I set an interval time to reload the quote of 25 seconds.
-Background color changes when the button is pressed or when the interval time elapses.
-*/
+// Function to change quote//
+//timer to change quote every 13 seconds//
 let timer;
 
 function quoteTimer() {
@@ -60,40 +55,32 @@ function quoteTimer() {
 
 function randomBackgroundColor() {
   let newBackground = randomColor();
-  //Sets a new background color, then matches it to the quote button
+  //I understood we werent suppposed to touch the css files which is why// 
+  //im trying to change the background from here, but it wont change//
   document.body.style.background = newBackground;
   document.getElementById("loadQuote").style.backgroundColor = newBackground;
 }
-
+//this function should display a different quote when icon is clicked//
 function printQuote() {
   let html = '';
   let randomQuote =  getRandomQuote();
 
   randomBackgroundColor();
   quoteTimer();
-
+//only created randomQuote/Source functions because my quotes didnt have years//
   html += '<p class="quote">' + randomQuote.quote + '</p>';
   html += '<p class="source">' + randomQuote.source;
   html += '</p>';
 
-//The variable div is being set to the quote box ID.
-//Then the innerHTML is changed to the html string we compiled.
-//Then we return that div.
+//https://www.w3schools.com/jsref/jsref_return.asp//
   let div = document.getElementById('quote-box');
   div.innerHTML = html;
   return div
 }
 
-
-
 /***
-  When the "Show another quote" button is clicked, the event listener
-  below will be triggered, and it will call, or "invoke", the `printQuote`
-  function. So do not make any changes to the line of code below this
+  So do not make any changes to the line of code below this
   comment.
 ***/
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
